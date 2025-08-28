45c89ba6656951f09b99aaa023c4ed4de88b5e30
George <george.alexander.wright@gmail.com>
2025-08-28 17:03:55 +0100

    start off group implementation

--- copycat/structures/objects/group.py
+++ copycat/structures/objects/group.py
@@ -2,4 +2,45 @@ from copycat.structures.object import Object
 
 
 class Group(Object):
-    pass
+    def __init__(self):
+        self.bond_descriptions = []
+        self.objects = []
+
+    def __len__(self):
+        return len(self.objects)
+
+    def __eq__(self, other):
+        return (
+            self.left_position,
+            self.right_position,
+            self.group_category,
+            self.direction_category,
+        ) == (
+            other.left_position,
+            other.right_position,
+            other.group_category,
+            other.direction_category,
+        )
+
+    def __contains__(self):
+        pass
+
+    @property
+    def leftmost_letter():
+        pass
+
+    @property
+    def rightmost_letter():
+        pass
+
+    def is_leftmost_in_string() -> bool:
+        pass
+
+    def is_rightmost_in_string() -> bool:
+        pass
+
+    def get_left_neighbour():
+        pass
+
+    def get_right_neighbour():
+        pass
