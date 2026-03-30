1e16353086f32b81c32e71e2490dc24d10d59379
George <george.alexander.wright@gmail.com>
2026-03-30 18:19:41 +0100

    test left and right neighbours

--- tests/unit/test_workspace_object.py
+++ tests/unit/test_workspace_object.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 import pytest
 
 from copycat.workspace_object import WorkspaceObject
@@ -10,3 +12,28 @@ def test_has_recursive_group_member_equivalent_to_object_equality():
     assert o2.has_recursive_group_member(o2)
     assert not o1.has_recursive_group_member(o2)
     assert not o2.has_recursive_group_member(o1)
+
+
+def test_left_and_right_neighbours():
+    string = SimpleNamespace()
+    l1 = WorkspaceObject(string=string, left_position=0, right_position=0)
+    l2 = WorkspaceObject(string=string, left_position=1, right_position=1)
+    l3 = WorkspaceObject(string=string, left_position=2, right_position=2)
+    l4 = WorkspaceObject(string=string, left_position=3, right_position=3)
+    g1 = WorkspaceObject(string=string, left_position=0, right_position=1)
+    g2 = WorkspaceObject(string=string, left_position=2, right_position=3)
+    string.objects = [l1, l2, l3, l4, g1, g2]
+
+    assert set(l1.left_neighbours) == set()
+    assert set(l2.left_neighbours) == {l1}
+    assert set(l3.left_neighbours) == {l2, g1}
+    assert set(l4.left_neighbours) == {l3}
+    assert set(g1.left_neighbours) == set()
+    assert set(g2.left_neighbours) == {l2, g1}
+
+    assert set(l1.right_neighbours) == {l2}
+    assert set(l2.right_neighbours) == {l3, g2}
+    assert set(l3.right_neighbours) == {l4}
+    assert set(l4.right_neighbours) == set()
+    assert set(g1.right_neighbours) == {l3, g2}
+    assert set(g2.right_neighbours) == set()
