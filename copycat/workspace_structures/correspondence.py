af8282fba942be575de4d4ddfb6ccedb86485849
George <george.alexander.wright@gmail.com>
2026-03-31 17:30:03 +0100

    implement correspondence supports and is incompatible with

--- copycat/workspace_structures/correspondence.py
+++ copycat/workspace_structures/correspondence.py
@@ -17,12 +17,29 @@ class Correspondence(WorkspaceStructure):
         return len(self.from_object) + len(self.to_object)
 
     def supports(self, other: Correspondence) -> bool:
-        """Returns True if self and other are not incompatible and if self has a concept mapping that supports the concept mappings of other."""
-        pass
+        """Returns True if self and other are not incompatible
+        and self has s concept mapping that supports other's concept mappings.
+        """
+        if self.from_object == other.from_object or self.to_object == other.to_object:
+            return False
+        if self.is_incompatible_with(other):
+            return False
+        for mapping_1 in self.get_distinguishing_mappings():
+            for mapping_2 in other.get_distinguishing_mappings():
+                if mapping_1.supports(mapping_2):
+                    return True
+        return False
 
-    def incompatible_with(self, other: Correspondence) -> bool:
-        """Returns True if self and other share objects or if self has a concept mapping that is incompatible with the concept mappings of other."""
-        pass
+    def is_incompatible_with(self, other: Correspondence) -> bool:
+        """Returns True if self and other share objects
+        or if self has a concept mapping incompatible with other's concept mappings."""
+        if self.from_object == other.from_object or self.to_object == other.to_object:
+            return True
+        for mapping_1 in self.get_distinguishing_mappings():
+            for mapping_2 in other.get_distinguishing_mappings():
+                if mapping_1.is_incompatible_with(mapping_2):
+                    return True
+        return False
 
     def calculate_internal_strength(self) -> float:
         relevant_distinguishing_mappings = self.get_relevant_distinguishing_mappings()
@@ -41,11 +58,17 @@ class Correspondence(WorkspaceStructure):
             average_strength * internal_coherence_factor * number_of_mappings_factor,
         )
 
+    def get_relevant_mappings(self) -> List[ConceptMapping]:
+        return [m for m in self.concept_mappings if m.is_relevant()]
+
+    def get_distinguishing_mappings(self) -> List[ConceptMapping]:
+        return [m for m in self.concept_mappings if m.is_distinguishing()]
+
     def get_relevant_distinguishing_mappings(self) -> List[ConceptMapping]:
         return [
-            mapping
-            for mapping in self.concept_mappings
-            if mapping.is_relevant() and mapping.is_distinguishing()
+            m
+            for m in self.concept_mappings
+            if m.is_relevant() and m.is_distinguishing()
         ]
 
     def is_internally_coherent(self) -> bool:
@@ -79,6 +102,6 @@ class Correspondence(WorkspaceStructure):
             return 1.0
         support_sum = 0.0
         for c in self.workspace.correspondences:
-            if c is not self and self.is_supported_by(c):
+            if c is not self and self.supports(c):
                 support_sum += c.total_strength
         return min(1.0, support_sum)
--- tests/unit/workspace_structures/test_correspondence.py
+++ tests/unit/workspace_structures/test_correspondence.py
@@ -5,6 +5,54 @@ import pytest
 from copycat.workspace_structures import Correspondence
 
 
+def test_supports_and_is_incompatible_with():
+    mapping_1 = SimpleNamespace()
+    mapping_1.is_distinguishing = lambda: True
+
+    mapping_2 = SimpleNamespace()
+    mapping_2.is_distinguishing = lambda: True
+    mapping_1.supports = lambda other: other == mapping_2
+    mapping_1.is_incompatible_with = lambda other: other != mapping_2
+
+    mapping_3 = SimpleNamespace()
+    mapping_3.is_distinguishing = lambda: True
+
+    correspondence_1 = Correspondence(
+        from_object="A",
+        to_object="B",
+        concept_mappings=[mapping_1],
+    )
+    correspondence_2 = Correspondence(
+        from_object="C",
+        to_object="D",
+        concept_mappings=[mapping_2],
+    )
+    correspondence_3 = Correspondence(
+        from_object="C",
+        to_object="D",
+        concept_mappings=[mapping_3],
+    )
+    correspondence_4 = Correspondence(
+        from_object="A",
+        to_object="B",
+        concept_mappings=[],
+    )
+    correspondence_5 = Correspondence(
+        from_object="A",
+        to_object="D",
+        concept_mappings=[],
+    )
+
+    assert correspondence_1.supports(correspondence_2) is True
+    assert correspondence_1.is_incompatible_with(correspondence_2) is False
+    assert correspondence_1.supports(correspondence_3) is False
+    assert correspondence_1.is_incompatible_with(correspondence_3) is True
+    assert correspondence_1.supports(correspondence_4) is False
+    assert correspondence_1.is_incompatible_with(correspondence_4) is True
+    assert correspondence_1.supports(correspondence_5) is False
+    assert correspondence_1.is_incompatible_with(correspondence_5) is True
+
+
 def test_get_relevant_distinguishing_mappings():
     distinguishing_not_relevant = SimpleNamespace()
     distinguishing_not_relevant.is_distinguishing = lambda: True
@@ -28,12 +76,20 @@ def test_get_relevant_distinguishing_mappings():
         ],
     )
 
+    assert correspondence.get_relevant_mappings() == [
+        relevant_not_distinguishing,
+        relevant_distinguishing,
+    ]
+    assert correspondence.get_distinguishing_mappings() == [
+        distinguishing_not_relevant,
+        relevant_distinguishing,
+    ]
     assert correspondence.get_relevant_distinguishing_mappings() == [
         relevant_distinguishing
     ]
 
 
-def test_is_inherently_coherent():
+def test_is_internally_coherent():
     distinguishing_not_relevant = SimpleNamespace()
     distinguishing_not_relevant.is_distinguishing = lambda: True
     distinguishing_not_relevant.is_relevant = lambda: False
@@ -68,3 +124,29 @@ def test_is_inherently_coherent():
     )
 
     assert correspondence.is_internally_coherent()
+
+
+def test_calculate_internal_strength():
+    relevant_distinguishing = SimpleNamespace()
+    relevant_distinguishing.is_distinguishing = lambda: True
+    relevant_distinguishing.is_relevant = lambda: True
+    relevant_distinguishing.strength = 0.5
+
+    correspondence = Correspondence(
+        from_object=None,
+        to_object=None,
+        concept_mappings=[relevant_distinguishing],
+    )
+
+    assert correspondence.calculate_internal_strength() == 0.4
+
+    second_relevant_distinguishing = SimpleNamespace()
+    second_relevant_distinguishing.is_distinguishing = lambda: True
+    second_relevant_distinguishing.is_relevant = lambda: True
+    second_relevant_distinguishing.strength = 0.8
+    relevant_distinguishing.supports = (
+        lambda other: other == second_relevant_distinguishing
+    )
+    correspondence.concept_mappings.append(second_relevant_distinguishing)
+
+    assert correspondence.calculate_internal_strength() == 1.0
