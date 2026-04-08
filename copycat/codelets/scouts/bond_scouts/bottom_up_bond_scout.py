c63d39326c27de33a25faf9e3ea4b6e9a6acb818
George <george.alexander.wright@gmail.com>
2026-04-08 17:04:19 +0100

    move helpers into parent class

--- copycat/codelets/scouts/bond_scouts/bottom_up_bond_scout.py
+++ copycat/codelets/scouts/bond_scouts/bottom_up_bond_scout.py
@@ -2,7 +2,6 @@ import random
 from typing import Optional
 
 from copycat.codelets.scouts.bond_scout import BondScout
-from copycat.codelets.strength_testers import BondStrengthTester
 from copycat.slipnet import Slipnode
 from copycat.workspace_structures.bond import Bond
 
@@ -40,70 +39,4 @@ class BottomUpBondScout(BondScout):
             bond_facet,
             from_obj_descriptor,
             to_obj_descriptor,
-        )
-
-    def _choose_bond_facet(self, from_obj, to_obj) -> Optional[Slipnode]:
-        from_obj_bond_facets = [
-            d.facet
-            for d in from_obj.descriptions
-            if d.facet.category.name == "bond_facet"
-        ]
-        to_obj_bond_facets = [
-            d.facet
-            for d in to_obj.descriptions
-            if d.facet.category.name == "bond_facet"
-        ]
-        common_bond_facets = [
-            facet for facet in from_obj_bond_facets if facet in to_obj_bond_facets
-        ]
-        if not common_bond_facets:
-            return None
-        supports = [
-            facet.get_total_description_type_support(from_obj.string)
-            for facet in common_bond_facets
-        ]
-        return random.choices(common_bond_facets, weights=supports, k=1)[0]
-
-    def _get_bond_category(
-        self, from_node: Slipnode, to_node: Slipnode
-    ) -> Optional[Slipnode]:
-        if from_node == to_node:
-            return self.slipnet.node_index_lookup["sameness"]
-        for link in from_node.outgoing_links:
-            if link.to_node == to_node:
-                return link.label
-        return None
-
-    def propose_bond(
-        self,
-        from_obj,
-        to_obj,
-        bond_category,
-        bond_facet,
-        from_obj_descriptor,
-        to_obj_descriptor,
-    ):
-        self.slipnet.activate_node_from_workspace(from_obj_descriptor.name)
-        self.slipnet.activate_node_from_workspace(to_obj_descriptor.name)
-        self.slipnet.activate_node_from_workspace(bond_facet.name)
-        proposed_bond = Bond(
-            from_obj,
-            to_obj,
-            bond_category,
-            bond_facet,
-            from_obj_descriptor,
-            to_obj_descriptor,
-        )
-        proposed_bond.proposal_level = 1
-        from_obj.string.add_proposed_bond(proposed_bond)
-        urgency = bond_category.bond_degree_of_association
-        urgency_bin = self.coderack.get_urgency_level_from_activation(urgency)
-        self.coderack.post(
-            BondStrengthTester(
-                urgency_bin=urgency_bin,
-                coderack=self.coderack,
-                slipnet=self.slipnet,
-                workspace=self.workspace,
-                proposed_bond=proposed_bond,
-            )
         )
