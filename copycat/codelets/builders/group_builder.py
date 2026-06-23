7656444980e1bdc416ac554b522e0dfcdcff825f
George <george.alexander.wright@gmail.com>
2026-06-23 15:39:57 +0100

    implement group builder init and test group strength tester

--- copycat/codelets/builders/group_builder.py
+++ copycat/codelets/builders/group_builder.py
@@ -1,4 +1,5 @@
 from copycat.codelets.builder import Builder
+from copycat.workspace_objects import Group
 
 
 class GroupBuilder(Builder):
@@ -6,4 +7,19 @@ class GroupBuilder(Builder):
     It fights with competitors if necessary.
     """
 
-    pass
+    def __init__(
+        self,
+        urgency_bin: int,
+        coderack: "Coderack",
+        workspace: "Workspace",
+        slipnet: "Slipnet",
+        proposed_group: Group,
+    ):
+        super().__init__(
+            urgency_bin=urgency_bin,
+            coderack=coderack,
+            workspace=workspace,
+            slipnet=slipnet,
+            proposed_structure=proposed_group,
+        )
+        self.proposed_group = proposed_group
---
+++ tests/unit/codelets/strength_testers/test_group_strength_tester.py
@@ -0,0 +1,56 @@
+from unittest.mock import Mock
+
+import pytest
+
+from copycat.codelets.strength_testers import GroupStrengthTester
+
+
+def test_run():
+    class MockCoderack:
+        def __init__(self):
+            self.posted_codelets = []
+            self.post_called = 0
+
+        def post(self, codelet):
+            self.post_called += 1
+            self.posted_codelets.append(codelet)
+
+        def get_urgency_level_from_activation(self, activation):
+            return 0
+
+    class MockSlipnet:
+        def __init__(self):
+            self.activate_called = 0
+
+        def activate_node_from_workspace(self, name):
+            self.activate_called += 1
+
+    class MockWorkspace:
+        pass
+
+    coderack = MockCoderack()
+    slipnet = MockSlipnet()
+    workspace = MockWorkspace()
+
+    proposed_group = Mock()
+    strength_tester = GroupStrengthTester(
+        urgency_bin=0,
+        coderack=coderack,
+        slipnet=slipnet,
+        workspace=workspace,
+        proposed_group=proposed_group,
+    )
+
+    # weak group should not post a builder
+    proposed_group.total_strength = 0.0
+    strength_tester.run(temperature=0.0)
+    assert slipnet.activate_called == 0
+    assert coderack.post_called == 0
+
+    # strong group should post a builder
+    slipnet.activate_called = 0
+    coderack.post_called = 0
+    proposed_group.total_strength = 1.0
+    strength_tester.run(temperature=0.0)
+    assert slipnet.activate_called == 2
+    assert coderack.post_called == 1
