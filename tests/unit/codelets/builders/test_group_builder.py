ca5645f90c57429ae2bf0cad9ccfc23455a469e4
George <george.alexander.wright@gmail.com>
2026-06-28 13:12:08 +0100

    test group builder fizzles if group exists

---
+++ tests/unit/codelets/builders/test_group_builder.py
@@ -0,0 +1,80 @@
+from unittest.mock import Mock
+
+import pytest
+
+from copycat.codelets.builders import group_builder
+from copycat.codelets.builders import GroupBuilder
+
+
+class MockSlipnet:
+    def __init__(self):
+        self.activate_node_from_workspace_called = 0
+
+    def activate_node_from_workspace(self, name):
+        self.activate_node_from_workspace_called += 1
+
+
+class MockWorkspace:
+    def __init__(self):
+        self.objects = []
+        self.break_bond_called = 0
+        self.break_group_called = 0
+        self.break_correspondence_called = 0
+
+    def break_bond(self, bond):
+        self.break_bond_called += 1
+
+    def break_group(self, group):
+        self.break_group_called += 1
+
+    def break_correspondence(self, correspondence):
+        self.break_correspondence_called += 1
+
+
+class MockWorkspaceString:
+    def __init__(self, groups):
+        self.groups = groups
+        self.add_bond_called = 0
+
+    def add_group(self, group):
+        self.add_group_called += 1
+        self.groups.append(group)
+
+    def delete_proposed_group(self, group):
+        pass
+
+
+def test_transfers_descriptions_and_fizzles_if_group_exists():
+    workspace = MockWorkspace()
+    slipnet = MockSlipnet()
+    coderack = Mock()
+    proposed_group = Mock()
+    proposed_group.descriptions = [Mock()]
+    existing_group = Mock()
+    existing_group.descriptions = [Mock()]
+    existing_group.has_description = lambda description: False
+    existing_group.add_description_called = 0
+    existing_group.add_description = lambda description: setattr(
+        existing_group,
+        "add_description_called",
+        existing_group.add_description_called + 1,
+    )
+
+    # Simulate that the group already exists in the workspace
+    workspace_string = MockWorkspaceString(groups=[])
+    workspace_string.get_group_if_present = lambda group: existing_group
+    proposed_group.string = workspace_string
+
+    builder = GroupBuilder(
+        urgency_bin=0,
+        coderack=coderack,
+        workspace=workspace,
+        slipnet=slipnet,
+        proposed_group=proposed_group,
+    )
+
+    # Run the builder
+    builder.run(temperature=0.5)
+
+    assert slipnet.activate_node_from_workspace_called == 1
+    assert existing_group.add_description_called == len(proposed_group.descriptions)
