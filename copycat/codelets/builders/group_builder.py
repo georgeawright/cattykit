51b1aac596af6721eba7f346c959d6a5203a867b
George <george.alexander.wright@gmail.com>
2026-06-28 12:46:07 +0100

    finish initial implementation of group builder

--- copycat/codelets/builders/group_builder.py
+++ copycat/codelets/builders/group_builder.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from copycat.codelets.builder import Builder
+from copycat.concept_mapping import ConceptMapping
 from copycat.tools import structure_beats_structures
 from copycat.workspace_objects import Group
 
@@ -81,6 +84,7 @@ class GroupBuilder(Builder):
         self._break_incompatible_structures(
             incompatible_groups, incompatible_correspondences
         )
+        self._flip_bonds(bonds_to_be_flipped)
         self._build_group()
 
     def _activate_group_descriptors(self, group: Group):
@@ -112,27 +116,81 @@ class GroupBuilder(Builder):
         ]
 
     def _get_incompatible_correspondences(self):
-        incompatible_correspondences = []
-        for obj in self.proposed_group.objects:
-            if obj.correspondence and self._is_incompatible_correspondence(
-                obj.correspondence
-            ):
-                incompatible_correspondences.append(obj.correspondence)
-        return incompatible_correspondences
+        return [
+            obj.correspondence
+            for obj in self.proposed_group.objects
+            if obj.correspondence
+            and self._correspondence_is_incompatible(obj.correspondence, obj)
+        ]
 
-    def _is_incompatible_correspondence(self, correspondence):
-        # TODO
-        pass
+    def _correspondence_is_incompatible(self, correspondence, obj):
+        """Returns True if the given correspondence is incompatible
+        with the proposed group."""
+        string_position_category_concept_mapping = next(
+            (
+                mapping
+                for mapping in correspondence.concept_mappings
+                if mapping.description_type_1.name == "string_position_category"
+            ),
+            None,
+        )
+        if string_position_category_concept_mapping is None:
+            return False
+        other_obj = correspondence.get_other_object(obj)
+        other_bond = None
+        if other_obj.is_leftmost_in_string():
+            other_bond = other_obj.right_bond
+        elif other_obj.is_rightmost_in_string():
+            other_bond = other_obj.left_bond
+        if other_bond is None or other_bond.direction_category is None:
+            return False
+        group_concept_mapping = ConceptMapping(
+            description_type_1=self.slipnet.direction_category,
+            description_type_2=self.slipnet.direction_category,
+            descriptor_1=self.proposed_group.direction_category,
+            descriptor_2=other_bond.direction_category,
+            label=None,
+            object_1=None,
+            object_2=None,
+        )
+        return self.workspace.incompatible_concept_mappings(
+            group_concept_mapping, string_position_category_concept_mapping
+        )
 
     def _break_incompatible_structures(
         self, incompatible_groups, incompatible_correspondences
     ):
-        # TODO
         for group in incompatible_groups:
             self.workspace.break_group(group)
         for correspondence in incompatible_correspondences:
             self.workspace.break_correspondence(correspondence)
 
+    def _flip_bonds(self, bonds_to_be_flipped: List["Bond"]):
+        """Flip any bonds that need flipping,
+        and replace in group's bond-list any bonds that got rebuilt
+        (and thus are equal but not eq to the corresponding bond
+        in the group's bond list)."""
+        if not bonds_to_be_flipped:
+            return
+        for bond in self.proposed_group.bonds:
+            flipped_bond = self.proposed_group.string.get_bond_if_present(
+                bond.get_flipped_version()
+            )
+            if flipped_bond:
+                self.workspace.break_bond(flipped_bond)
+                self.workspace.build_bond(bond)
+            else:
+                existing_bond = self.proposed_group.string.get_bond_if_present(bond)
+                if existing_bond and existing_bond != bond:
+                    index = self.proposed_group.bonds.index(bond)
+                    self.proposed_group.bonds[index] = existing_bond
+
     def _build_group(self):
-        # TODO
-        pass
+        string = self.proposed_group.string
+        string.add_group(self.proposed_group)
+        for obj in self.proposed_group.objects:
+            obj.group = self.proposed_group
+        for bond in self.proposed_group.bonds:
+            bond.group = self.proposed_group
+        for description in self.proposed_group.descriptions:
+            self.slipnet.activate_node_from_workspace(description.descriptor.name)
