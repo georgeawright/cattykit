53e24f02468fb916d4633f9b670c9bd20ca89324
George <george.alexander.wright@gmail.com>
2026-07-03 11:46:39 +0100

    implement propose correspondence

--- copycat/codelets/scouts/correspondence_scout.py
+++ copycat/codelets/scouts/correspondence_scout.py
@@ -1,6 +1,8 @@
 from typing import List
 
+from copycat.workspace_structures import Correspondence
 from copycat.codelets.scout import Scout
+from copycat.codelets.strength_testers import CorrespondenceStrengthTester
 from copycat.concept_mapping import ConceptMapping
 
 
@@ -16,4 +18,28 @@ class CorrespondenceScout(Scout):
         concept_mappings: List[ConceptMapping],
         object_2_flipped: bool,
     ):
-        pass
+        proposed_correspondence = Correspondence(
+            self.workspace, object_1, object_2, concept_mappings
+        )
+        proposed_correspondence.proposal_level = 1
+        distinguishing_mappings = proposed_correspondence.get_distinguishing_mappings()
+        for mapping in distinguishing_mappings:
+            self.slipnet.activate_node_from_workspace(mapping.description_type_1.name)
+            self.slipnet.activate_node_from_workspace(mapping.descriptor_1.name)
+            self.slipnet.activate_node_from_workspace(mapping.description_type_2.name)
+            self.slipnet.activate_node_from_workspace(mapping.descriptor_2.name)
+        self.workspace.add_proposed_correspondence(proposed_correspondence)
+        urgency = sum(mapping.strength for mapping in distinguishing_mappings) / len(
+            distinguishing_mappings
+        )
+        urgency_bin = self.coderack.get_urgency_level_from_activation(urgency)
+        self.coderack.post(
+            CorrespondenceStrengthTester(
+                urgency_bin=urgency_bin,
+                coderack=self.coderack,
+                slipnet=self.slipnet,
+                workspace=self.workspace,
+                proposed_correspondence=proposed_correspondence,
+                object_2_flipped=object_2_flipped,
+            )
+        )
--- copycat/codelets/strength_testers/correspondence_strength_tester.py
+++ copycat/codelets/strength_testers/correspondence_strength_tester.py
@@ -6,4 +6,18 @@ class CorrespondenceStrengthTester(StrengthTester):
     It probabilistically posts a correspondence builder with urgency a function of strength.
     """
 
-    pass
+    def __init__(
+        self,
+        urgency_bin,
+        coderack,
+        slipnet,
+        workspace,
+        proposed_correspondence,
+        object_2_flipped=False,
+    ):
+        super().__init__(urgency_bin, coderack, slipnet, workspace)
+        self.proposed_correspondence = proposed_correspondence
+        self.object_2_flipped = object_2_flipped
+
+    def run(self, temperature: float):
+        pass
