918ba51193a657ba20d1e6713555078cc4aed712
George <george.alexander.wright@gmail.com>
2026-07-21 13:23:05 +0100

    implement correspondence strength tester (untested)

--- copycat/codelets/strength_testers/correspondence_strength_tester.py
+++ copycat/codelets/strength_testers/correspondence_strength_tester.py
@@ -1,4 +1,9 @@
+import random
+
+from copycat.codelet_result import CodeletResult, Finish, Fizzle, FizzleReason
+from copycat.codelets.builders.correspondence_builder import CorrespondenceBuilder
 from copycat.codelets.strength_tester import StrengthTester
+from copycat.tools import temperature_adjust
 
 
 class CorrespondenceStrengthTester(StrengthTester):
@@ -21,5 +26,40 @@ class CorrespondenceStrengthTester(StrengthTester):
         self.proposed_correspondence = proposed_correspondence
         self.object_2_flipped = object_2_flipped
 
-    def run(self, temperature: float):
-        pass
+    def run(self, temperature: float) -> CodeletResult:
+        if self.proposed_correspondence.object_1 not in self.workspace.objects:
+            return Fizzle(FizzleReason.OBJECTS_NO_LONGER_EXIST)
+        if (
+            self.proposed_correspondence.object_2 not in self.workspace.objects
+            and not (
+                self.object_2_flipped
+                and self.proposed_correspondence.obj2.get_flipped_version()
+                in self.workspace.target_string.objects
+            )
+        ):
+            return Fizzle(FizzleReason.OBJECTS_NO_LONGER_EXIST)
+        self.proposed_correspondence.update_strength_values()
+        build_probability = temperature_adjust(
+            self.proposed_correspondence.total_strength / 100, temperature
+        )
+        if random.random() > build_probability:
+            self.workspace.delete_proposed_correspondence(self.proposed_correspondence)
+            return Fizzle(FizzleReason.PROPOSED_STRUCTURE_TOO_WEAK)
+        for mapping in self.proposed_correspondence.concept_mapping_list:
+            mapping.description_type_1.activate_from_workspace()
+            mapping.descriptor_1.activate_from_workspace()
+            mapping.description_type_2.activate_from_workspace()
+            mapping.descriptor_2.activate_from_workspace()
+        urgency = self.proposed_correspondence.total_strength
+        urgency_bin = self.coderack.get_urgency_level_from_activation(urgency)
+        self.coderack.post(
+            CorrespondenceBuilder(
+                urgency_bin=urgency_bin,
+                coderack=self.coderack,
+                slipnet=self.slipnet,
+                workspace=self.workspace,
+                proposed_correspondence=self.proposed_correspondence,
+                object_2_flipped=self.object_2_flipped,
+            )
+        )
+        return Finish()
