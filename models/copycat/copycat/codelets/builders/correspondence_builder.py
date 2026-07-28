43f19eaafd21572e52a10b7ab089e3e3cbcf680e
George <george.alexander.wright@gmail.com>
2026-07-28 16:57:00 +0100

    implement correspondence to bond and group incompatibility checks

--- models/copycat/copycat/codelets/builders/correspondence_builder.py
+++ models/copycat/copycat/codelets/builders/correspondence_builder.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from copycat.codelet_result import CodeletResult, Finish, Fizzle, FizzleReason
 from copycat.codelets.builder import Builder
+from copycat.concept_mapping import ConceptMapping
 from copycat.tools import structure_beats_structures
 from copycat.workspace_objects import Group, Letter
 from copycat.workspace_structures import Correspondence
@@ -31,67 +34,59 @@ class CorrespondenceBuilder(Builder):
     def run(self, temperature: float) -> "CodeletResult":
         if self.proposed_correspondence.from_object not in self.workspace.objects:
             return Fizzle(FizzleReason.OBJECTS_NO_LONGER_EXIST)
-        if (
-            self.proposed_correspondence.to_object not in self.workspace.objects
-            and not (
-                self.to_object_flipped
-                and self.proposed_correspondence.to_object.get_flipped_version()
-                in self.workspace.objects
-            )
-        ):
-            return Fizzle(FizzleReason.OBJECTS_NO_LONGER_EXIST)
+        if self.proposed_correspondence.to_object not in self.workspace.objects:
+            if self.to_object_flipped:
+                existing_target = self.workspace.get_group_if_present(
+                    self.proposed_correspondence.to_object.get_flipped_version()
+                )
+                if existing_target is None:
+                    return Fizzle(FizzleReason.OBJECTS_NO_LONGER_EXIST)
+            else:
+                return Fizzle(FizzleReason.OBJECTS_NO_LONGER_EXIST)
         self.workspace.delete_proposed_correspondence(self.proposed_correspondence)
         if self._augment_existing_correspondence_if_exists():
             return Fizzle(FizzleReason.STRUCTURE_ALREADY_EXISTS)
         if self._not_all_concept_mappings_relevant():
             return Fizzle(FizzleReason.NOT_ALL_CONCEPT_MAPPINGS_RELEVANT)
         incompatible_correspondences = self._get_incompatible_correspondences()
-        if incompatible_correspondences:
+        for incompatible_correspondence in incompatible_correspondences:
+            # larger groups should be preferred over smaller groups and letters
+            fight_result = structure_beats_structures(
+                self.proposed_correspondence,
+                len(self.proposed_correspondence),
+                [incompatible_correspondence],
+                len(incompatible_correspondence),
+                temperature=temperature,
+            )
+            if not fight_result:
+                return Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
+        incompatible_bond = self._get_incompatible_bond()
+        if incompatible_bond:
+            fight_result = structure_beats_structures(
+                self.proposed_correspondence,
+                3,
+                [incompatible_bond],
+                2,
+                temperature=temperature,
+            )
+            if not fight_result:
+                return Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
+        incompatible_group = incompatible_bond.group if incompatible_bond else None
+        if incompatible_group:
             fight_result = structure_beats_structures(
                 self.proposed_correspondence,
                 1,
-                incompatible_correspondences,
+                [incompatible_group],
                 1,
                 temperature=temperature,
             )
             if not fight_result:
                 return Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
-        (
-            incompatible_bonds,
-            incompatible_groups,
-        ) = self._get_incompatible_bonds_and_groups()
-        if incompatible_bonds:
+        if self.to_object_flipped:
             fight_result = structure_beats_structures(
                 self.proposed_correspondence,
                 1,
-                incompatible_bonds,
-                1,
-                temperature=temperature,
-            )
-            if not fight_result:
-                return Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
-        if incompatible_groups:
-            fight_result = structure_beats_structures(
-                self.proposed_correspondence,
-                1,
-                incompatible_groups,
-                max([g.letter_span for g in incompatible_groups]),
-                temperature=temperature,
-            )
-            if not fight_result:
-                return Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
-        existing_group = (
-            self.workspace.target_string.get_group_if_present(
-                self.proposed_correspondence.to_object.get_flipped_version()
-            )
-            if self.to_object_flipped
-            else None
-        )
-        if self.to_object_flipped and existing_group:
-            fight_result = structure_beats_structures(
-                self.proposed_correspondence,
-                1,
-                [existing_group],
+                [existing_target],
                 1,
                 temperature=temperature,
             )
@@ -108,15 +103,15 @@ class CorrespondenceBuilder(Builder):
             )
             if not fight_result:
                 return Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
-        for group in incompatible_groups:
-            self.workspace.break_group(group)
-        for bond in incompatible_bonds:
-            self.workspace.break_bond(bond)
+        if incompatible_group:
+            self.workspace.break_group(incompatible_group)
+        if incompatible_bond:
+            self.workspace.break_bond(incompatible_bond)
         for correspondence in incompatible_correspondences:
             self.workspace.break_correspondence(correspondence)
-        if existing_group:
-            self.workspace.break_group(existing_group)
-            for bond in existing_group.bonds:
+        if self.to_object_flipped:
+            self.workspace.break_group(existing_target)
+            for bond in existing_target.bonds:
                 self.workspace.break_bond(bond)
             for bond in self.proposed_correspondence.to_object.bonds:
                 self.workspace.target_string.add_bond(bond)
@@ -156,8 +151,38 @@ class CorrespondenceBuilder(Builder):
             or c.is_incompatible_boundarywise_with(self.proposed_correspondence)
         ]
 
-    def _get_incompatible_bonds_and_groups(self):
-        pass
+    def _get_incompatible_bond(self) -> Optional["Bond"]:
+        source_bond = (
+            self.proposed_correspondence.from_object.right_bond
+            if self.proposed_correspondence.from_object.is_leftmost_in_string()
+            else self.proposed_correspondence.from_object.left_bond
+            if self.proposed_correspondence.from_object.is_rightmost_in_string()
+            else None
+        )
+        target_bond = (
+            self.proposed_correspondence.to_object.right_bond
+            if self.proposed_correspondence.to_object.is_leftmost_in_string()
+            else self.proposed_correspondence.to_object.left_bond
+            if self.proposed_correspondence.to_object.is_rightmost_in_string()
+            else None
+        )
+        if not (
+            source_bond
+            and target_bond
+            and source_bond.direction_category
+            and target_bond.direction_category
+        ):
+            return None
+        bond_concept_mapping = ConceptMapping(
+            self.slipnet["direction_category"],
+            self.slipnet["direction_category"],
+            source_bond.direction_category,
+            target_bond.direction_category,
+        )
+        for mapping in self.proposed_correspondence.concept_mappings:
+            if bond_concept_mapping.is_incompatible_with(mapping):
+                return target_bond
+        return None
 
     def _get_incompatible_rule(self):
         pass
