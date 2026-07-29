6b7a5a6cb66d52816782519fc9a1f12c919f6513
George <george.alexander.wright@gmail.com>
2026-07-29 15:40:40 +0100

    implement extrinsic description

---
+++ models/copycat/copycat/workspace_structures/extrinsic_description.py
@@ -0,0 +1,23 @@
+from copycat.slipnode import Slipnode
+from copycat.workspace_object import WorkspaceObject
+from copycat.workspace_structure import WorkspaceStructure
+
+
+class ExtrinsicDescription(WorkspaceStructure):
+    """A description with respect to another object in the workspace.
+    For example in "abc -> abd", the 'd' could be "successor of the 'c'"
+    d ~ ExtrinsicDescription(successor, letter-category, c)."""
+
+    def __init__(
+        self,
+        relation: Slipnode,
+        description_type_related: Slipnode,
+        other_object: WorkspaceObject,
+    ):
+        self.relation = relation
+        self.description_type_related = description_type_related
+        self.other_object = other_object
+
+    @property
+    def conceptual_depth(self):
+        return self.relation.conceptual_depth
