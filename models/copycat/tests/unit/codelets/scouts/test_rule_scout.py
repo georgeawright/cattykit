0d5c09acd4f1879ea389cafe7c805f2171392624
George <george.alexander.wright@gmail.com>
2026-07-29 16:20:32 +0100

    test raises exception if more than one changed letter

--- models/copycat/tests/unit/codelets/scouts/test_rule_scout.py
+++ models/copycat/tests/unit/codelets/scouts/test_rule_scout.py
@@ -16,3 +16,15 @@ def test_fizzles_if_not_all_initial_string_letters_have_replacements():
     result = rule_scout.run(temperature=0.0)
 
     assert result == Fizzle(FizzleReason.NOT_ALL_REPLACEMENTS_FOUND)
+
+
+def test_raises_exception_if_more_than_one_changed_letter():
+    workspace = Mock()
+    workspace.all_replacements_found.return_value = True
+    workspace.initial_string.get_changed_objects.return_value = [Mock(), Mock()]
+
+    rule_scout = RuleScout(
+        urgency_bin=0, coderack=Mock(), workspace=workspace, slipnet=Mock()
+    )
+    with pytest.raises(Exception):
+        result = rule_scout.run(temperature=0.0)
