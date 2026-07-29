120efef8eddcb3f84bd5a12c772a1837497bf519
George <george.alexander.wright@gmail.com>
2026-07-29 20:09:19 +0100

    test fizzles if existing rule wins

--- models/copycat/tests/unit/codelets/builders/test_rule_builder.py
+++ models/copycat/tests/unit/codelets/builders/test_rule_builder.py
@@ -12,11 +12,36 @@ class MockWorkspace:
         self.rule = None
 
 
+class MockSlipnet:
+    def __init__(self):
+        self.activate_called = 0
+
+    def activate_node_from_workspace(self, name):
+        self.activate_called += 1
+
+
 def test_fizzles_if_rule_already_exist():
+    slipnet = MockSlipnet()
     workspace = MockWorkspace()
     rule = Mock()
     workspace.rule = rule
-    builder = RuleBuilder(Mock(), Mock(), workspace, Mock(), rule)
+    builder = RuleBuilder(Mock(), Mock(), workspace, slipnet, rule)
     result = builder.run(temperature=0.0)
 
     assert result == Fizzle(FizzleReason.RULE_ALREADY_EXISTS)
+    assert slipnet.activate_called == 3
+
+
+def test_fizzles_if_existing_rule_wins(monkeypatch):
+    slipnet = MockSlipnet()
+    workspace = MockWorkspace()
+    workspace.rule = Mock()
+    rule = Mock()
+    builder = RuleBuilder(Mock(), Mock(), workspace, slipnet, rule)
+    monkeypatch.setattr(
+        rule_builder, "structure_beats_structures", lambda *_, **__: False
+    )
+    result = builder.run(temperature=0.0)
+
+    assert result == Fizzle(FizzleReason.INCOMPATIBLE_STRUCTURES_WON)
+    assert slipnet.activate_called == 0
