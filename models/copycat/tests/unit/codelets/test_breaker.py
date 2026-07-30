02c9c253a2900755c71ca8e6609fa98446b2de4d
George <george.alexander.wright@gmail.com>
2026-07-30 17:00:49 +0100

    test breaker fizzles

---
+++ models/copycat/tests/unit/codelets/test_breaker.py
@@ -0,0 +1,23 @@
+from unittest.mock import Mock
+
+import pytest
+
+from copycat.codelet_result import Finish, Fizzle, FizzleReason
+from copycat.codelets import Breaker
+
+
+def test_fizzles_if_temperature_is_too_low():
+    breaker = Breaker(Mock(), Mock(), Mock(), Mock())
+    result = breaker.run(temperature=0.0)
+
+    assert result == Fizzle(FizzleReason.TEMPERATURE_TOO_LOW)
+
+
+def test_fizzles_if_there_are_no_structures():
+    workspace = Mock()
+    workspace.structures = []
+
+    breaker = Breaker(Mock(), Mock(), workspace, Mock())
+    result = breaker.run(temperature=1.0)
+
+    assert result == Fizzle(FizzleReason.NO_STRUCTURES)
