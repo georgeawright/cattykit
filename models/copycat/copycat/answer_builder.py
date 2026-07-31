1573b1ce318a040dbebcb02de2f5af28a9a87037
George <george.alexander.wright@gmail.com>
2026-07-31 10:35:35 +0100

    implement get objects to change

--- models/copycat/copycat/answer_builder.py
+++ models/copycat/copycat/answer_builder.py
@@ -6,6 +6,7 @@ from copycat.snag_exception import SnagException
 from copycat.workspace import Workspace
 from copycat.workspace_object import WorkspaceObject
 from copycat.workspace_objects import Letter
+from copycat.workspace_structures import Description
 
 
 class AnswerBuilder:
@@ -28,6 +29,43 @@ class AnswerBuilder:
     def _get_objects_to_change(self) -> List[WorkspaceObject]:
         if not self.workspace.translated_rule.specifies_change():
             return []
+        objects_to_change = []
+        rule_facet = self.workspace.translated_rule.descriptor_1_facet
+        rule_descriptor = self.workspace.translated_rule.descriptor_1
+        rule_obj_category = self.workspace.translated_rule.object_category_1
+        for obj in self.workspace.target_string.objects:
+            if obj.get_descriptor(self.slipnet["object_category"]) != rule_obj_category:
+                continue
+            if obj.get_descriptor(rule_facet) == rule_descriptor:
+                objects_to_change.append(obj)
+                continue
+            tester = self.workspace.translated_rule.descriptor_1.description_tester
+            if tester is not None and tester(obj):
+                description = Description(obj, rule_facet, rule_descriptor)
+                obj.add_description()
+                objects_to_change.append(obj)
+        if (
+            rule_facet != self.slipnet["string_position_category"]
+            and len(objects_to_change) <= 1
+        ):
+            return objects_to_change
+        changed_object_correspondence = next(
+            obj for obj in self.workspace.initial_string.objects if obj.is_changed
+        ).correspondence
+        if (
+            changed_object_correspondence is not None
+            and changed_object_correspondence.target in objects_to_change
+        ):
+            return [changed_object_correspondence.target]
+        return next(
+            obj
+            for obj in objects_to_change
+            if (
+                obj.group is None
+                or obj.group.get_descriptor(self.slipnet["string_position_category"])
+                != rule_descriptor
+            )
+        )
 
     def _get_modified_letters(
         self, obj: WorkspaceObject, description_type: Slipnode
