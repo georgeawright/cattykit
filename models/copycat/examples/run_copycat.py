dd3c6318d36818dc193ce7651574da3f438664bb
George <george.alexander.wright@gmail.com>
2026-08-11 14:56:22 +0100

    fix copycat example run

---
+++ models/copycat/examples/run_copycat.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from cattykit.logging import PrintLogger
+from copycat import Copycat
+
+_CONFIG_DIRECTORY = Path(__file__).parent.parent / "configs"
+
+
+def main() -> None:
+    logger = PrintLogger()
+    copycat = Copycat.from_json(
+        str(_CONFIG_DIRECTORY / "slipnet.json"),
+        str(_CONFIG_DIRECTORY / "coderack.json"),
+        str(_CONFIG_DIRECTORY / "hyperparameters.json"),
+        logger=logger,
+    )
+    copycat.solve("abc -> abd ==> ijk -> ?")
+    logger.close()
+
+
+if __name__ == "__main__":
+    main()
