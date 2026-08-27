55be1820705886535bbaac06039d08e7c87e76f4
George <george.alexander.wright@gmail.com>
2026-08-27 16:24:47 +0100

    use underscore, not hyphen in slipnode names

--- models/copycat/copycat/workspace_structures/correspondence.py
+++ models/copycat/copycat/workspace_structures/correspondence.py
@@ -37,8 +37,8 @@ class Correspondence(WorkspaceStructure):
             (
                 mapping.label
                 for mapping in self.concept_mappings
-                if mapping.description_type_1.name == "direction-category"
-                and mapping.description_type_2.name == "direction-category"
+                if mapping.description_type_1.name == "direction_category"
+                and mapping.description_type_2.name == "direction_category"
             ),
             None,
         )
