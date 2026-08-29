eb3cf3c6a64a16a6fa2bcaab98fb0d2823886da8
George <george.alexander.wright@gmail.com>
2026-08-29 23:55:26 +0100

    tidier description testers

--- models/copycat/copycat/copycat.py
+++ models/copycat/copycat/copycat.py
@@ -42,13 +42,12 @@ DESCRIPTION_TESTERS = {
     "is_single": lambda x: isinstance(x, Letter) and x.spans_whole_string(),
     "is_whole": lambda x: isinstance(x, Group) and x.spans_whole_string(),
     # ALPHABETIC POSITION
-    "is_first": lambda x: x.get_descriptor_with_facet_name("letter_category").name
-    == "a"
-    if x.get_descriptor_with_facet_name("letter_category") is not None
-    else False,
-    "is_last": lambda x: x.get_descriptor_with_facet_name("letter_category").name == "z"
-    if x.get_descriptor_with_facet_name("letter_category") is not None
-    else False,
+    "is_first": lambda x: (lambda d: d.name == "a" if d is not None else False)(
+        x.descriptor_with_facet_name("letter_category")
+    ),
+    "is_last": lambda x: (lambda d: d.name == "z" if d is not None else False)(
+        x.descriptor_with_facet_name("letter_category")
+    ),
     # OBJECT TYPE
     "is_letter_object": lambda x: isinstance(x, Letter),
     "is_group_object": lambda x: isinstance(x, Group),
