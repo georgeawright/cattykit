e5558d8613c0b23e329044c6dd546fd905f92e72
George <george.alexander.wright@gmail.com>
2026-08-29 12:01:18 +0100

    post low temperature rule translators to high bin, not extremely high bin

--- models/copycat/copycat/workspace.py
+++ models/copycat/copycat/workspace.py
@@ -606,7 +606,7 @@ class Workspace:
             for _ in range(
                 coderack.number_of_codelets_to_post("translated-rule", workspace=self)
             ):
-                urgency_bin = 2 if temperature > 0.25 else 6
+                urgency_bin = 2 if temperature > 0.25 else 4
                 codelets.append(
                     RuleTranslator(
                         urgency_bin=urgency_bin,
